341f37bdaf2be816314107a69f6e56804b7428e5
Grzegorz Fira <120904447+grfira@users.noreply.github.com>
2024-02-03 21:08:10 +0100

    Product test (#2)
    product test

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,36 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (price == null) {
+            throw new IllegalArgumentException("Price cannot be NULL!");
+        }
+        if (price.signum() < 0) {
+            throw new IllegalArgumentException("Price cannot be NEGATIVE!");
+        }
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be NULL!");
+        }
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Name cannot be empty!");
+        }
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return price.add(price.multiply(taxPercent));
     }
 }
